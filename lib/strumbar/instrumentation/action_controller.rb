ecfc9071b3b13d679da65612d2fc16de2bf603e1
Matt Wilson <mhw@hypomodern.com>
2013-02-01 22:51:02 -0500

    Use more concise, clear code, thx @cadwallion.

--- lib/strumbar/instrumentation/action_controller.rb
+++ lib/strumbar/instrumentation/action_controller.rb
@@ -7,15 +7,11 @@ module Strumbar
 
         Strumbar.subscribe /process_action.action_controller/ do |client, event|
           key = "#{event.payload[:controller]}.#{event.payload[:action]}"
-          db_runtime = if using_mongoid
-            event.payload[:mongo_runtime]
-          else
-            event.payload[:db_runtime]
-          end
+          db_runtime_key = using_mongoid ? :mongo_runtime : :db_runtime
 
           client.timing     "#{key}.total_time", event.duration, options[:rate]
           client.timing     "#{key}.view_time",  event.payload[:view_runtime], options[:rate]
-          client.timing     "#{key}.db_time",    db_runtime, options[:rate]
+          client.timing     "#{key}.db_time",    event.payload[db_runtime_key], options[:rate]
 
           client.increment  "#{key}.status.#{event.payload[:status]}", options[:rate]
         end
